4fda95b485d5664a19a0c1770d2abbea432124e0
Pilseong <lal4995@gmail.com>
2024-05-10 18:03:32 +0900

    feat: fix unique_data_preprocessing.py by ypilseong

--- src/data_preprocessing_review/data_preprocessing/trans_to_xy.py
+++ src/data_preprocessing_review/data_preprocessing/trans_to_xy.py
@@ -15,7 +15,7 @@ def get_addrs(x):
 
 
 # 국토교통부 geocoder open api 
-def geocoing(store_ad, type):
+def geocoding(store_ad, type):
     try:
 
         apiurl = "https://api.vworld.kr/req/address?"
@@ -42,13 +42,7 @@ def geocoing(store_ad, type):
 
 if __name__=='__main__':
 
-    df = pd.read_csv('data/store_data.csv', sep=',', encoding='utf-8')
-    df = df[['사업장명', '업종구분소분류','인허가일자', '상세영업상태명', '소재지전체주소', '도로명전체주소']]
-    df.columns = ['store_name', 'business_category', 'permission_date', 'situation', 'address', 'address_road']
-    df = df.loc[df.situation=='영업']
-    df = df[df['business_category'] != '편의점']
-    df.reset_index(drop=True, inplace=True)
-
+    df = pd.read_csv('data/filtered_data.csv', sep=',', encoding='utf-8')
     df['trans_ad'] = df['address'].apply(get_addrs)
     df['trans_ad_road'] = df['address_road'].apply(get_addrs)
 
@@ -60,12 +54,14 @@ if __name__=='__main__':
     for i in range(len(df)):
         address = df.loc[i]['trans_ad_road']
         try:
-            x_value, y_value = geocoing(address, type="road")
+            x_value, y_value = geocoding(address, type="road")
             if x_value is not None and y_value is not None:
                 df.at[i, 'x'] = x_value
                 df.at[i, 'y'] = y_value
+                success_count += 1
             else:
                 print(f"Failed to geocode address: {address}, skipping to the next one.")
+                success_count += 1
                 continue  # 데이터를 찾을 수 없으면 다음 번호로 넘어감
         except Exception as e:
             print(f"An error occurred at index {i}: {e}")
@@ -73,16 +69,15 @@ if __name__=='__main__':
     
     print(f"Successfully processed {success_count} addresses.")
     
-    
 
     nulls = df[df['y'] == 0]
     null_index = list(nulls.index)
 
     for i in null_index:
         address = df.loc[i]['trans_ad']
-        x_value, y_value = geocoing(address, type="parsel")
+        x_value, y_value = geocoding(address, type="parsel")
         df.at[i, 'x'] = x_value
         df.at[i, 'y'] = y_value
 
 
-    df.to_csv('data/geopy_address3.csv', encoding='utf-8')
+    df.to_csv('data/geopy_address6.csv', encoding='utf-8')
